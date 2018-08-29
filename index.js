0b8a1802b4b9fcf051094ded987d62f41bcddb07
Andrew Zubiri <zubiri@gmail.com>
2018-08-29 00:21:10 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,23 +10,33 @@ function handleFormSubmit(event) {
 
 function fetchCurrentWeather(city) {
   //fetch current weather based on city
+  console.log("=== event ===")
+
 }
 
 function displayCurrentWeather(json) {
   //render current weather data to the DOM using provided IDs and json from API
+  console.log("=== event ===")
+
 }
 
 
 function fetchFiveDayForecast(city) {
   //fetch five day forecast data based on city
+  console.log("=== event ===")
+
 }
 
 function displayFiveDayForecast(json) {
   //render five day forecast data to the DOM using provided IDs and json from API
+  console.log("=== event ===")
+
 }
 
 function createChart(json) {
   //Bonus: render temperature chart using five day forecast data and ChartJS
+  console.log("=== event ===")
+
 }
 
 document.addEventListener('DOMContentLoaded', function() {
