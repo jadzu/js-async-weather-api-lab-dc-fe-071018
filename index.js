0b313fec685d6f9a4e5daebfb1c7ba876b50d653
Andrew Zubiri <zubiri@gmail.com>
2018-08-29 00:21:31 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,19 +4,19 @@ const API_KEY = "691086dddf7db5cb26c413f8f28f1954"
 
 function handleFormSubmit(event) {
   //handle submit event
-  console.log("=== event ===")
+  console.log("=== handleFormSubmit ===")
   document.addEventListener()
 }
 
 function fetchCurrentWeather(city) {
   //fetch current weather based on city
-  console.log("=== event ===")
+  console.log("=== fetchCurrentWeather ===")
 
 }
 
 function displayCurrentWeather(json) {
   //render current weather data to the DOM using provided IDs and json from API
-  console.log("=== event ===")
+  console.log("=== displayCurrentWeather ===")
 
 }
 
